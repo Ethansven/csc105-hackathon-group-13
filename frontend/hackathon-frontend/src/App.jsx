5c9d768857a7b9febafc7f4ddb5fcd675e756fd9
APOM-Tawan <pathompong.cham@mail.kmutt.ac.th>
2023-05-27 14:05:04 +0700

    second

--- frontend/hackathon-frontend/src/App.jsx
+++ frontend/hackathon-frontend/src/App.jsx
@@ -1,33 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import login from "../src/assets/page/login"
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useNavigate,
+  Router,
+} from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Axios from "./axiosInstance";
+import Login from './assets/page/login'
+import Register from '../src/assets/page/register'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <ToastContainer />
+        <Login/>
+        {/* <Register/> */}
+        {/* <Routes>
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+        </Routes> */}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ frontend/hackathon-frontend/src/assets/page/login.jsx
@@ -0,0 +1,170 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import BG from "../components/video/OceanBG.mp4";
+import Logo from "../components/pic/FUNTIME.png";
+import Axios from "../../axiosInstance";
+import { useState } from "react";
+import {  toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+const theme = createTheme();
+
+export default function SignInSide() {
+  const [name,setname]=useState();
+const [pass,setpass]=useState();
+    const login= async()=>{
+      event.preventDefault();
+      const user = await Axios.post("/login",{
+        username:name,
+        password:pass,
+      });
+      if (user.data.success==true) {
+      console.log("hi");
+    }else{
+      toast.error(user.data.message);
+    }
+    }
+  
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Grid
+        container
+        component="main"
+        sx={{
+          height: "100vh",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <CssBaseline />
+        <Grid item xs={12} sm={8} md={5}>
+          <Paper
+            elevation={6}
+            sx={{
+              my: 8,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              padding: "30px",
+              borderRadius: "20px",
+              color:"white",
+              backgroundColor: "rgba(255, 255, 255, 0.5)",
+              boxShadow: "0px 2px 8px rgba(0, 0, 0, 0.2)",
+            }}
+          >
+            <Box
+              component="form"
+              // noValidate
+              onSubmit={login}
+              sx={{
+                mt: 1,
+                width: "100%",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+                <img style={{width:"50%",padding:"5px"}} src={Logo} alt="logo" />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                label="Username"
+                value={name}
+                type="text"
+                onChange={(e) => setname(e.target.value)}
+                sx={{
+                  borderRadius: 8,
+                  backgroundColor: "rgba(255, 255, 255, 0.5)",
+                  "& fieldset": { border: "none" },
+                  boxShadow: "0px 2px 8px rgba(0, 0, 0, 0.2)",
+                }}
+              />
+              <TextField
+                margin="normal"
+                required
+                label="Password"
+                fullWidth
+                value={pass}
+                type="password"
+                onChange={(e) => setpass(e.target.value)}
+                sx={{
+                  borderRadius: 8,
+                  backgroundColor: "rgba(255, 255, 255, 0.5)",
+                  marginTop: 4,
+                  "& fieldset": { border: "none" },
+                  boxShadow: "0px 2px 8px rgba(0, 0, 0, 0.2)",
+                }}
+              />
+              <Button
+                type="submit"
+                variant="contained"
+                fullWidth
+                sx={{
+                  mt: 3,
+                  mb: 2,
+                  color: "#50422C",
+                  backgroundColor: "white",
+                  borderRadius: 20,
+                  padding: 2,
+                  alignContent: "center",
+                  width: "50%",
+                  "&:hover": {
+                    backgroundColor: "#C0C0C0",
+                    color: "white",
+                    boxShadow: "0px 0px 15px 2px rgba(0, 0, 0, 0.3)",
+                  },
+                }}
+              >
+                Sign In
+              </Button>
+              <Grid item sx={{marginLeft:2,}}>
+                  <Link href="#" variant="body2" style={{ color: "white" }}>
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
+              <Grid container sx={{ marginTop: 3 }}>
+                <Grid>
+                  <div
+                    style={{
+                      position: "fixed",
+                      top: 0,
+                      left: 0,
+                      width: "100%",
+                      height: "100%",
+                      overflow: "hidden",
+                      zIndex: -1,
+                      textAlign: "center",
+                    }}
+                  >
+                    <video
+                      autoPlay
+                      loop
+                      muted
+                      style={{
+                        width: "100%",
+                        height: "100%",
+                        objectFit: "cover",
+                      }}
+                    >
+                      <source src={BG} type="video/mp4" />
+                    </video>
+                  </div>
+                </Grid>
+              </Grid>
+            </Box>
+          </Paper>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
+  );
+}
